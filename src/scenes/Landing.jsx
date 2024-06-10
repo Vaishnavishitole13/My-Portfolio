2c5ff23ee95ca72f4dc83ebe8085c13dba958a11
Vaishnavi Shitole <vaishnavishitole745.com>
2024-06-10 13:22:21 +0530

    first commit

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,100 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import profile from "../assets/profile-image.png";
+import { MY_NAME, RESUME_LINK } from "../constants/data";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10 "
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-2/5 z-10 mt-16 md:mt-20 flex justify-center md:order-2">
+        {isAboveLarge ? (
+          <div>
+            <img alt="profile" src={profile} />
+          </div>
+        ) : (
+          <img alt="profile" src={profile} />
+        )}
+      </div>
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            {MY_NAME}
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Experienced as a application developer with a strong background in React.js,
+            JavaScript, SQL, Unix, XSL, XML, Jira, and GitHub. Successfully contributed
+            3 years in project, including on-site production support. Proven ability to deliver
+            high-quality solutions in a dynamic environment.
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <a
+            href={RESUME_LINK}
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+            hover:bg-blue hover:text-white transition duration-500"
+            target="_blank"
+          >
+            My Resume
+          </a>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("projects")}
+            href="#projects"
+          >
+            <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
+              View My Work
+            </div>
+          </AnchorLink>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
